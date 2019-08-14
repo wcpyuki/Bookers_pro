478abc898ad077df68c1fe6ea71ddd63259948a4
wcpyuki <gkyuki03111458@gmail.com>
2019-08-14 16:48:15 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   attachment :profile_image
-   has_many :books, dependent: :destroy
+   has_many :books, :post_images, dependent: :destroy
 
 	validates :name, presence: true,  length: { in: 2..20 }
 	validates :introduction,  length: { maximum: 50 }
